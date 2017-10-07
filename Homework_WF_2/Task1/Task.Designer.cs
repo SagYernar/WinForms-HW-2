e94a61856254ea180728d50f421f599029e0e925
SagYernar <sagyernar@gmail.com>
2017-10-07 23:53:57 +0600

    All done!
    Олег, координаты мыши относительно формы получены!!!

---
+++ Homework_WF_2/Task1/Task.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Task
+{
+    partial class Task
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.infoLabel = new System.Windows.Forms.Label();
+            this.widthLabel = new System.Windows.Forms.Label();
+            this.HeghtLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // infoLabel
+            // 
+            this.infoLabel.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.infoLabel.AutoSize = true;
+            this.infoLabel.Location = new System.Drawing.Point(67, 9);
+            this.infoLabel.Name = "infoLabel";
+            this.infoLabel.Size = new System.Drawing.Size(0, 13);
+            this.infoLabel.TabIndex = 0;
+            // 
+            // widthLabel
+            // 
+            this.widthLabel.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.widthLabel.AutoSize = true;
+            this.widthLabel.Location = new System.Drawing.Point(46, 47);
+            this.widthLabel.Name = "widthLabel";
+            this.widthLabel.Size = new System.Drawing.Size(0, 13);
+            this.widthLabel.TabIndex = 1;
+            // 
+            // HeghtLabel
+            // 
+            this.HeghtLabel.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.HeghtLabel.AutoSize = true;
+            this.HeghtLabel.Location = new System.Drawing.Point(158, 47);
+            this.HeghtLabel.Name = "HeghtLabel";
+            this.HeghtLabel.Size = new System.Drawing.Size(0, 13);
+            this.HeghtLabel.TabIndex = 2;
+            // 
+            // Task
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.HeghtLabel);
+            this.Controls.Add(this.widthLabel);
+            this.Controls.Add(this.infoLabel);
+            this.Name = "Task";
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Task_MouseClick);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Task1_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label infoLabel;
+        private System.Windows.Forms.Label widthLabel;
+        private System.Windows.Forms.Label HeghtLabel;
+    }
+}
+
---
+++ Homework_WF_2/Task1/Task.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Задание 3 Представьте, что у вас на форме есть прямоугольник, 
+//границы которого на 10 пикселей отстоят от границ рабочей области формы.
+//Необходимо создать следующие обработчики:
+
+//■ Обработчик нажатия левой кнопки мыши, который выводит сообщение о том, 
+//где находится текущая точка: вну- три прямоугольника, снаружи, на границе прямоугольника.
+//Домашнее задание №1 Домашнее задание № Если при нажатии левой кнопки мыши была нажата кнопка Control (Ctrl),
+//то приложение должно закрываться; 
+
+//■ Обработчик нажатия правой кнопки мыши, который выводит в заголовок окна информацию
+// о размере клиентской(рабочей) области окна в виде: Ширина = x, Высота = y,
+// где x и y – соответствующие параметры вашего окна; 
+
+//■ Обработчик перемещения указателя мыши в пределах рабочей области,
+//который должен выводить в заголовок окна текущие координаты мыши x и y
+
+namespace Task
+{
+    public partial class Task : Form
+    {
+        private int otstup = 20;
+        private int borderX = 17;
+        private int borderY = 40;
+        public Task()
+        {
+            InitializeComponent();
+        }
+
+        private void Task_MouseClick(object sender, MouseEventArgs e)
+        {
+            if ((Control.ModifierKeys & Keys.Control) == Keys.Control)
+            {
+                Close();
+            }
+
+            if ( e.Location.X < otstup || e.Location.X > this.Size.Width - otstup - borderX ||
+                 e.Location.Y < otstup || e.Location.Y > this.Size.Height - otstup - borderY )
+            {
+                infoLabel.Text = "Снаружи прямоугольника";
+            }
+            else if(e.Location.X == otstup || e.Location.X == this.Size.Width - otstup - borderX ||
+                    e.Location.Y == otstup || e.Location.Y == this.Size.Height - otstup - borderY)
+            {
+                infoLabel.Text = "На границе прямоугольника";
+            }
+            else
+            {
+                infoLabel.Text = "Внутри прямоугольника";
+            }
+        }
+
+        private void Task1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Text = e.Location.ToString();
+            widthLabel.Text = "Ширина = " + this.Size.Width.ToString();
+            HeghtLabel.Text = "Высота = " + this.Size.Height.ToString();
+        }
+    }
+}
